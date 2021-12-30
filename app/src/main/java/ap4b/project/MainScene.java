eff1f34c0cea1ead89a2867202a20d222482f3b9
Adrien Burgun <adrien.burgun@orange.fr>
2021-12-30 15:40:40 +0100

    :sparkles: CTM (which is super inefficient)

--- app/src/main/java/ap4b/project/MainScene.java
+++ app/src/main/java/ap4b/project/MainScene.java
@@ -11,6 +11,7 @@ import javafx.scene.image.Image;
 import javafx.geometry.*;
 import javafx.beans.value.*;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class MainScene extends Scene {
     private Canvas canvas;
@@ -23,8 +24,63 @@ public class MainScene extends Scene {
     private final static int tileSize = 16;
 
     private HashMap<String, Image> images = new HashMap();
+    private HashSet<String> is_ctm = new HashSet();
 
     private String tmpMap[][] = new String[10][10];
+
+    private void drawTile(int x, int y, String tile) {
+        Image image = this.images.get(tile);
+
+        if (is_ctm.contains(tile)) {
+            ctx.drawImage(
+                image,
+                tileSize,
+                tileSize,
+                tileSize,
+                tileSize,
+                x * tileSize * zoomFactor,
+                y * tileSize * zoomFactor,
+                tileSize * zoomFactor,
+                tileSize * zoomFactor
+            );
+        } else {
+            ctx.drawImage(
+                image,
+                x * tileSize * zoomFactor,
+                y * tileSize * zoomFactor,
+                tileSize * zoomFactor,
+                tileSize * zoomFactor
+            );
+        }
+    }
+
+    // This is awfully inefficient, but we weren't given enough time
+    private void drawTileCTM(int x, int y, String[][] map) {
+        int width = map.length;
+        int height = map[0].length;
+
+        if (!is_ctm.contains(map[x][y])) return;
+
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                if (dx == 0 && dy == 0) continue;
+                if (x + dx < 0 | x + dx >= width || y + dy < 0 || y + dy >= height) continue;
+                if (map[x + dx][y + dy] == map[x][y]) continue;
+
+                ctx.drawImage(
+                    this.images.get(map[x][y]),
+                    (dx + 1) * tileSize,
+                    (dy + 1) * tileSize,
+                    tileSize,
+                    tileSize,
+                    (x + dx) * tileSize * zoomFactor,
+                    (y + dy) * tileSize * zoomFactor,
+                    tileSize * zoomFactor,
+                    tileSize * zoomFactor
+                );
+            }
+        }
+    }
 
     private void draw() {
         ctx.clearRect(0, 0, this.width, this.height);
@@ -32,14 +88,13 @@ public class MainScene extends Scene {
 
         for (int y = 0; y < 10; y++) {
             for (int x = 0; x < 10; x++) {
-                // TODO: transitions between biomes
-                ctx.drawImage(
-                    this.images.get(this.tmpMap[x][y]),
-                    x * tileSize * zoomFactor,
-                    y * tileSize * zoomFactor,
-                    tileSize * zoomFactor,
-                    tileSize * zoomFactor
-                );
+                drawTile(x, y, this.tmpMap[x][y]);
+            }
+        }
+
+        for (int y = 0; y < 10; y++) {
+            for (int x = 0; x < 10; x++) {
+                drawTileCTM(x, y, this.tmpMap);
             }
         }
     }
@@ -60,6 +115,7 @@ public class MainScene extends Scene {
 
         this.images.put("grass", new Image("/grass.png"));
         this.images.put("forest", new Image("/forest.png"));
+        this.is_ctm.add("forest");
 
         this.parentApp = parentApp;
         this.width = width;
