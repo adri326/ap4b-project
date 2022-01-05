8ab7ccc7799b37a2c69efb6057125321fd519af2
otchappi <85830004+otchappi@users.noreply.github.com>
2022-01-05 17:32:37 +0100

    Add files via upload

---
+++ app/src/main/java/ap4b/project/Restaurant.java
@@ -0,0 +1,19 @@
+package ap4b.project;
+
+public class Restaurant extends habitation
+{
+	public Restaurant()
+	{
+		this.consommationUnity=20;
+		this.taux_satisfaction=2;
+		this.consomationSpeed=700;
+	}
+	
+	public void addPopulation() {
+    	population+=20;
+    }
+    
+    public void reducePopulation() {
+    	population-=20;
+    }
+}
