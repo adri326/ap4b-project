c6fdde653b192ee09a468f727911dd48c7017d82
taragam21 <91080688+taragam21@users.noreply.github.com>
2022-01-07 11:30:53 +0100

    Update HydroelectricGenerator.java

--- app/src/main/java/ap4b/project/map/factory/generator/HydroelectricGenerator.java
+++ app/src/main/java/ap4b/project/map/factory/generator/HydroelectricGenerator.java
@@ -14,17 +14,8 @@ public class HydroelectricGenerator extends PowerGenerator{
     public boolean hasRequiredResources(){return false;}
 
     public void produce(Weather whe){
-        while (true) {
-            try {
-                Thread.sleep(speed*1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            float temp = storage.getStored(ResourceType.ENERGY) + 0.5f * whe.getIrrigationFactor();
-            this.storage.setStored(ResourceType.ENERGY, (int) temp);
-            this.pollution += 0.0000001;
-
-        }
+        float temp = storage.getStored(ResourceType.ENERGY) + 0.5f * whe.getIrrigationFactor();
+        this.storage.setStored(ResourceType.ENERGY, (int) temp);
+        this.pollution += 0.0000001;
     }
 }
