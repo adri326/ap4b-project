3f8df3e489d879152e07f7c7064f232a33a3b107
Adrien Burgun <adrien.burgun@orange.fr>
2022-01-07 23:54:50 +0100

    :bug: Set thorium's resource to THORIUM

--- app/src/main/java/ap4b/project/map/factory/resource/ThoriumMine.java
+++ app/src/main/java/ap4b/project/map/factory/resource/ThoriumMine.java
@@ -17,7 +17,7 @@ public class ThoriumMine extends ResourceGenerator{
         if (rawQuantity>0) {
             this.quantity += 200.0f;
             --this.rawQuantity;
-            this.storage.setStored(ResourceType.COAL, (int) this.quantity);
+            this.storage.setStored(ResourceType.THORIUM, (int) this.quantity);
         }
         else
             System.out.println("La resource premiere est epuise, pensez à changer de resource!! ");
