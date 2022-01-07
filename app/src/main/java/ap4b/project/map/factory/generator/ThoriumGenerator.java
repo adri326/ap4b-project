e6a8ee03a4dfd95147c965d8f24a2ff0d0b96138
taragam21 <91080688+taragam21@users.noreply.github.com>
2022-01-07 11:33:30 +0100

    Update ThoriumGenerator.java

--- app/src/main/java/ap4b/project/map/factory/generator/ThoriumGenerator.java
+++ app/src/main/java/ap4b/project/map/factory/generator/ThoriumGenerator.java
@@ -13,22 +13,13 @@ public class ThoriumGenerator extends PowerGenerator{
     }
     public boolean hasRequiredResources(){return true;}
     public void produce(Weather whe){
-        while (true) {
-            try {
-                Thread.sleep(speed*1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            if(thoM.quantity>0) {
-                float temp = storage.getStored(ResourceType.THORIUM) + 200.0f;
-                this.storage.setStored(ResourceType.THORIUM, (int) temp);
-                this.pollution += 0.001;
-                thoM.quantity-=100;
-            }
-            else
-                System.out.println("Impossible de produre, la quantite du thorium est insuffisante! ");
-
-        }
-    }
+       if(thoM.quantity>0) {
+          float temp = storage.getStored(ResourceType.THORIUM) + 200.0f;
+          this.storage.setStored(ResourceType.THORIUM, (int) temp);
+          this.pollution += 0.001;
+          thoM.quantity-=100;
+      }
+      else
+         System.out.println("Impossible de produre, la quantite du thorium est insuffisante! ");
+      }
 }
