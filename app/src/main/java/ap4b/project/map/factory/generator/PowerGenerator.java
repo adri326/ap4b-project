88c10c268d2376d28489c6bd1a206a7680fbf7ee
taragam21 <91080688+taragam21@users.noreply.github.com>
2022-01-07 11:32:04 +0100

    Update PowerGenerator.java

--- app/src/main/java/ap4b/project/map/factory/generator/PowerGenerator.java
+++ app/src/main/java/ap4b/project/map/factory/generator/PowerGenerator.java
@@ -5,8 +5,7 @@ public abstract class PowerGenerator extends Factory{
     public abstract void produce(Weather whe);
     public abstract boolean hasRequiredResources();
     public Upgrade getUpgrades(){
-        Upgrade up = new Upgrade();
-        return up;
+        return upgrade;
     }
 
 }
