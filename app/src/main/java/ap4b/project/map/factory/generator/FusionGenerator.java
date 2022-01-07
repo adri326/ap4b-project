8cd44ad9a81a6518fbf6011a7a96d2e8f14d74f1
taragam21 <91080688+taragam21@users.noreply.github.com>
2022-01-07 11:30:07 +0100

    Update FusionGenerator.java

--- app/src/main/java/ap4b/project/map/factory/generator/FusionGenerator.java
+++ app/src/main/java/ap4b/project/map/factory/generator/FusionGenerator.java
@@ -11,17 +11,8 @@ public class FusionGenerator extends PowerGenerator{
     }
     public boolean hasRequiredResources(){return true;}
     public void produce(Weather whe){
-        while (true) {
-            try {
-                Thread.sleep(speed*1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            float temp = storage.getStored(ResourceType.ENERGY) + 100.0f;
-            this.storage.setStored(ResourceType.ENERGY, (int) temp);
-            this.pollution += 0.0005;
-
-        }
+          float temp = storage.getStored(ResourceType.ENERGY) + 100.0f;
+          this.storage.setStored(ResourceType.ENERGY, (int) temp);
+          this.pollution += 0.0005;
     }
 }
