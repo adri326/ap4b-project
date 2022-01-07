e3adf5922924f8f3b54f199081890e99062ca1b2
taragam21 <91080688+taragam21@users.noreply.github.com>
2022-01-07 11:34:50 +0100

    Update UraniumGenerator.java

--- app/src/main/java/ap4b/project/map/factory/generator/UraniumGenerator.java
+++ app/src/main/java/ap4b/project/map/factory/generator/UraniumGenerator.java
@@ -13,22 +13,13 @@ public class UraniumGenerator extends PowerGenerator{
     }
     public boolean hasRequiredResources(){return true;}
     public void produce(Weather whe){
-        while (true) {
-            try {
-                Thread.sleep(speed*1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            if(uraM.quantity>0) {
-                float temp = storage.getStored(ResourceType.URANIUM) + 100.0f;
-                this.storage.setStored(ResourceType.URANIUM, (int) temp);
-                this.pollution += 0.009;
-                uraM.quantity-=100;
-            }
-            else
-                System.out.println("Impossible de produre, la quantite de l'uranium est insuffisante! ");
-
-        }
+         if(uraM.quantity>0) {
+             float temp = storage.getStored(ResourceType.URANIUM) + 100.0f;
+             this.storage.setStored(ResourceType.URANIUM, (int) temp);
+             this.pollution += 0.009;
+             uraM.quantity-=100;
+         }
+         else
+            System.out.println("Impossible de produre, la quantite de l'uranium est insuffisante! ");
     }
 }
