41996a4f015ebb652f42f938c85992d93b996405
taragam21 <91080688+taragam21@users.noreply.github.com>
2022-01-07 11:36:52 +0100

    Update WindGenerator.java

--- app/src/main/java/ap4b/project/map/factory/generator/WindGenerator.java
+++ app/src/main/java/ap4b/project/map/factory/generator/WindGenerator.java
@@ -15,17 +15,8 @@ public class WindGenerator extends PowerGenerator{
     public boolean hasRequiredResources(){return false;}
 
     public void produce(Weather whe){
-        while (true) {
-            try {
-                Thread.sleep(speed*1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            float temp = storage.getStored(ResourceType.ENERGY) + 0.1f * whe.getIrrigationFactor();
-            this.storage.setStored(ResourceType.ENERGY, (int) temp);
-            this.pollution += 0.0000000001;
-
-        }
+        float temp = storage.getStored(ResourceType.ENERGY) + 0.1f * whe.getIrrigationFactor();
+        this.storage.setStored(ResourceType.ENERGY, (int) temp);
+        this.pollution += 0.0000000001;
     }
 }
